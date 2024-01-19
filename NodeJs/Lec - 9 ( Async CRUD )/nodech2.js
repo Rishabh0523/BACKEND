557163e547a96049216b9688b6f1727bc2b0a48b
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2024-01-19 16:53:18 +0530

    Add files via upload

---
+++ NodeJs/Lec - 9 ( Async CRUD )/nodech2.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+// fs.mkdir('thapa' ,(err) =>{
+//     console.log('folder dreated');
+// });
+
+// fs.writeFile('bio.txt' , 'My name is Rishabh', (err) => {
+//     console.log('files created');
+// });
+
+// fs.appendFile('./thapa/bio.txt' , 'plz like and share my vidoe' , (err) => {
+//     console.log('files data appended');
+// });
+
+// fs.readFile('./thapa/bio.txt' ,'utf-8' , (err,data ) => {
+//     console.log(data)
+// });
+
+// fs.rename('./thapa/bio.txt' , 'mybio.txt' , (err) => {
+// console.log("rename done")
+// })
+
+// fs.unlink("./thapa/mybio.txt" , (err) => {
+// console.log("files deleted")
+// });
+
+// fs.rmdir('./thapa' ,( err) => {
+//     console.log('folder deleted')
+// });
