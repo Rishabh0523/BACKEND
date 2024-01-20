34f7910d9443ed99c5734e8b6c10235ba6b991c6
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2024-01-20 10:10:36 +0530

    Add files via upload

---
+++ NodeJs/Lec - 15 ( Modulewrapperfun )/index.js
@@ -0,0 +1,25 @@
+ 
+//  ( function (){
+
+//     const name1 = "Rishabh";
+//     console.log(name1)
+//  })
+
+
+//  const name1 = "Rishabh";
+//  console.log(name1)
+
+
+// Bonus
+
+
+
+( function () {
+
+    var a = 'Rishabh';
+    // console.log(a);
+
+})  (); 
+
+// console.log(a); we cannot get value it is public and in function all values are private
+
