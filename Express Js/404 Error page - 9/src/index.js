88bd14bfbc29522075e34279a91f2f3127850916
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2024-01-22 23:15:43 +0530

    Add files via upload

---
+++ Express Js/404 Error page - 9/src/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const hbs = require('hbs')
+
+const app =express();
+
+const port = 8000;
+
+// to set the view engine
+
+const templatePath = path.join(__dirname, './templates')
+const partialPath = path.join(__dirname, '../templates/partials')
+
+// How to chnange views name
+
+app.set('view engine' , 'hbs');
+app.set('views ' , templatePath);
+
+// template engine route
+
+app.get("",(req , res) => {
+    res.render(index);
+})
+
+
+// 404 Method
+
+app.get("*",(req , res) => {
+    res.render("404" , {
+        errorComponent :"oops page could not be found "
+    });
+})
+
+// app.use(express.static)
+
+app.get("/" , (req , res) => {
+    res.send("Hello everyone");
+})
+
+app.listen(port, () => {
+    console.log(`Server is listing at ${port}`)
+})
